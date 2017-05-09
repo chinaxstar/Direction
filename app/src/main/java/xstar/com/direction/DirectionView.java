923ae7205d530faa20ef7bb958d3995726babc04
xstar <xyx_xstar@sina.cn>
2017-05-09 16:45:38 +0800

    指南车

---
+++ app/src/main/java/xstar/com/direction/DirectionView.java
@@ -0,0 +1,200 @@
+package xstar.com.direction;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by xstar on 2016-12-01.
+ */
+public class DirectionView extends View {
+    public static final String TAG = "DirectionView";
+    private static final String[] directs = {"北", "东", "南", "西"};
+
+    public DirectionView(Context context) {
+        this(context, null);
+    }
+
+    public DirectionView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    private DisplayMetrics displayMetrics = new DisplayMetrics();
+
+    public DirectionView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        textSize = (int) (displayMetrics.scaledDensity * textSize);
+    }
+
+    Paint paint = new Paint();
+    private String text = getDirectDescription(0);
+    private int scale_len = 35;
+    private int directAngle = 0;
+    private int space = 8;
+    private int textSize = 25;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawCircle(canvas, scale_len);
+        drawLittleTriangle(canvas, directAngle, scale_len);
+        drawDirectText(canvas, text);
+    }
+
+    private void drawCircle(Canvas canvas, int len) {
+        initClockPaint();
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+        int half_w = width >> 1;
+        int half_h = height >> 1;
+        float radius = half_w - 20;//圆盘的半径
+        for (int i = 0; i < 360; i++) {
+            //画刻度之前，先把画布的状态保存下来
+            canvas.save();
+            //让画布旋转3/5度，参数一是需要旋转的度数，参数2,3是旋转的圆心
+            canvas.rotate(i, half_w, half_h);
+            //旋转后再圆上画上一长10dp的刻度线
+            canvas.drawLine(half_w, half_h - radius, half_w, half_h - radius + len, paint);
+            //恢复画布
+            canvas.restore();
+        }
+    }
+
+    private void initClockPaint() {
+        paint.setAntiAlias(true);//消除锯齿
+        paint.setColor(Color.WHITE);//设置圆盘画笔的颜色为红色
+        paint.setStyle(Paint.Style.STROKE);//设置画笔的类型为描边
+        paint.setStrokeWidth(1);//设置描边宽度
+        paint.setAlpha(100);//设置画笔透明度，最高值为255
+    }
+
+    private void drawLittleTriangle(Canvas canvas, int angle, int triangle_len) {
+        paint.setStyle(Paint.Style.FILL);
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+        int half_w = width >> 1;
+        int half_h = height >> 1;
+        int tl_half = triangle_len >> 1;
+        int tl_height = (int) (tl_half * Math.pow(3, 0.5));
+        int radius = computeCircle(canvas, 2);//圆盘的半径
+        canvas.save();
+        canvas.rotate(angle, half_w, half_h);
+        int start_y = half_h - radius;
+        Path path = new Path();
+        path.moveTo(half_w, start_y);
+        path.lineTo(half_w - tl_half, start_y + tl_height);
+        path.lineTo(half_w + tl_half, start_y + tl_height);
+        path.close();
+        canvas.drawPath(path, paint);
+        canvas.restore();
+    }
+
+    private void drawDirectText(Canvas canvas, String text) {
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+        int half_w = width >> 1;
+        int half_h = height >> 1;
+
+        int radius = computeCircle(canvas, 3);
+        for (int i = 0; i < 4; i++) {
+            canvas.save();
+            canvas.rotate(i * 90, half_w, half_h);
+            canvas.drawText(directs[i], 0, 1, half_w - (textSize / 2), half_h - radius + textSize, paint);
+            canvas.restore();
+        }
+
+        Rect textR = new Rect();
+        paint.setTextSize(textSize);
+        paint.getTextBounds(text, 0, text.length(), textR);
+        canvas.drawText(text, 0, text.length(), half_w - (textR.width() / 2), half_h - (textR.height() / 2), paint);
+        half_h += textR.height();
+//        drawAngleInfo(directAngle, canvas, half_w, half_h);
+    }
+
+    private void drawAngleInfo(int angle, Canvas canvas, int cx, int cy) {
+        Rect textR = new Rect();
+        while (angle > 90) angle = angle - 90;
+        text = angle + "度";
+        paint.setTextSize((float) (textSize * 0.82));
+        paint.getTextBounds(text, 0, text.length(), textR);
+        canvas.drawText(text, 0, text.length(), cx - (textR.width() / 2), cy - (textR.height() / 2), paint);
+    }
+
+    public int getScale_len() {
+        return scale_len;
+    }
+
+    public void setScale_len(int scale_len) {
+        this.scale_len = scale_len;
+        invalidate();
+    }
+
+    public int getDirectAngle() {
+        return directAngle;
+    }
+
+    public void setDirectAngle(int directAngle) {
+        this.directAngle = directAngle;
+        this.text = getDirectDescription(directAngle);
+        invalidate();
+    }
+
+    private String getDirectDescription(int directAngle) {
+        String text;
+        if (inRange(directAngle, 0, 10) && inRange(directAngle, 350, 360))
+            text = "正北";
+        else if (inRange(directAngle, 10, 80))
+            text = "东偏北";
+        else if (inRange(directAngle, 80, 100)) text = "正东";
+        else if (inRange(directAngle, 100, 170)) text = "东偏南";
+        else if (inRange(directAngle, 170, 190)) text = "正南";
+        else if (inRange(directAngle, 190, 260)) text = "西偏南";
+        else if (inRange(directAngle, 260, 280)) text = "正西";
+        else if (inRange(directAngle, 280, 350)) text = "西偏北";
+        else text = "未知方向";
+        return text;
+    }
+
+    private boolean inRange(int num, int x, int y) {
+        if (x > y) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        return x <= num && y >= num;
+    }
+
+    private int computeCircle(Canvas canvas, int floor) {
+        int width = canvas.getWidth();
+        int half_w = width >> 1;
+        int radius = 0;
+        switch (floor) {
+            case 1:
+                radius = half_w - 20;//圆盘的半径
+                break;
+            case 2:
+                radius = computeCircle(canvas, 1);
+                radius -= (scale_len + space);
+                break;
+            case 3:
+                radius = computeCircle(canvas, 2);
+                radius -= ((scale_len / 2 * Math.pow(3, 0.5)) + space);
+                break;
+            case 4:
+                radius = computeCircle(canvas, 3);
+                radius -= textSize + space;
+                break;
+
+        }
+        return radius;
+    }
+}
---
+++ app/src/main/java/xstar/com/direction/SunLocation.java
@@ -0,0 +1,120 @@
+package xstar.com.direction;
+
+import java.util.Calendar;
+
+/**
+ * Created by xstar on 2016-12-02.
+ */
+public class SunLocation
+{
+	public static final String	TAG	= "SunLocation";
+	// 平均日地距离 km
+	public static final double	R0	= 1.49597890E8;
+
+	/**
+	 * θ=2πt／365.2422 t=N－N0 N为积日 在年内的序号
+	 * N0=79.6764+0.2422×（年份－1985）－INT〔（年份－1985）／4
+	 * 
+	 * @param cita
+	 * @return
+	 */
+	public double getER(double cita)
+	{
+		return 1.000423 + 0.032359 * Math.sin(cita) + 0.000086 * Math.sin(2 * cita) - 0.008349 * Math.cos(cita) + 0.000115 * Math.cos(2 * cita);
+	}
+
+	// 太阳赤纬角
+	public double getED(double cita)
+	{
+		return 0.3723 + 23.2567 * Math.sin(cita) + 0.1149 * Math.sin(2 * cita) - 0.1712 * Math.sin(3 * cita) - 0.758 * Math.cos(cita) + 0.3656 * Math.cos(2 * cita) + 0.0201 * Math.cos(3 * cita);
+	}
+    // 太阳赤纬角
+	public double getED(int dayOfYear)
+	{
+		double b = 2 * Math.PI * (284+dayOfYear)/365;
+		return 23.45*Math.sin(b);
+	}
+
+	public double getET(double cita)
+	{
+		return 0.0028 - 1.9857 * Math.sin(cita) + 9.9059 * Math.sin(2 * cita) - 7.0924 * Math.cos(cita) - 0.6882 * Math.cos(2 * cita);
+	}
+
+	/**
+	 * 计算日角 自转产生的角度 同一时刻下 同一经度 不同纬度的角度相同
+	 */
+	public double getSunAngle(double JD, double JF, int NF, int Y, int R, int S, int F)
+	{
+		float A = NF / 4f;//年份 计算闰年
+		double K = 2 * Math.PI / 365.2422;//每小时的日角
+		double N0 = 79.6764 + 0.2422 * (NF - 1985) - (int) ((NF - 1985) / 4f);//
+		float B = A - (int) (A);
+		float C = 32.8f;
+		if (Y <= 2) C = 30.6f;//
+		if (B == 0 && Y > 2) C = 31.8f;//闰年月份大于2
+		int G = (int) (30.6 * Y - C + 0.5) + R;
+		double L = (JD + JF / 60) / 15;//经度/15 从0经度转到当前经度的时间
+		double H = S - 8 + F / 60d;//北京时间 -8 + 分钟/60
+		double N = G + (H - L) / 24d;//积日
+		double cita = (N - N0) * K;
+		return cita;
+	}
+
+	/**
+	 * 获取时角
+	 *
+	 * @param Sz
+	 *            太阳真实时
+	 * @param Fz
+	 *            太阳真实分
+	 * @return
+	 */
+	public double getSunHourAngle(double Sz, double Fz)
+	{
+		return (Sz + Fz / 60d - 12) * 15;
+	}
+
+	// 将北京时间转换成当地时间 每一经度相差约四分钟
+	public double getSunBeiJingTrue(int S, int F, double JD, double cita)
+	{
+		double Sd = S + Math.abs((F - ((120 - JD)) * 4)) / 60;
+		return Sd + getET(cita) / 60;
+	}
+
+	// 太阳高度（h⊙）的计算公式为
+	// sinh⊙=sinδsinφ＋cosδcosφcosτ（8）
+	// 式中，δ就是太阳赤纬角，即式（5）中的Ed，φ为当地的地理纬度，τ为当时的太阳时角。φ值不难获得，且一旦确定，不会改变。δ值的计算可以从前述程序中得到。唯一需要说明的是太阳时角的计算。其计算式为
+
+    //太阳高度角随着地方时和太阳的赤纬的变化而变化。太阳赤纬（与太阳直射点纬度相等）以δ表示，观测地地理纬度用φ表示（太阳赤纬与地理纬度都是北纬为正，南纬为负），地方时(时角)以t表示，有太阳高度角的计算公式：
+    //sin h=sin φ sin δ+cos φ cosδ cos t
+	public double getSunHigh(double ED, double weidu, double sunhourangle)
+	{
+		double sin = Math.sin(ED) * Math.sin(weidu) + Math.cos(ED) * Math.cos(weidu) * Math.cos(sunhourangle);
+		return Math.asin(sin) / 2 / Math.PI * 360;
+	}
+
+	public double getSunLocationAngle(double sunhight, double weidu, double ED)
+	{
+		double cosA = 0;
+		if (sunhight == 0)
+		{
+			cosA = -Math.sin(ED) / Math.cos(weidu);
+		}
+		else
+		{
+			cosA = (Math.sin(sunhight) * Math.sin(weidu) - Math.sin(ED)) / (Math.cos(sunhight) * Math.cos(weidu));
+		}
+		if (cosA <= 0)
+		{
+			// 90~180
+		}
+		else
+		{
+			// 0~90
+
+		}
+		return Math.acos(cosA) / 2 / Math.PI * 360;
+	}
+
+
+}
